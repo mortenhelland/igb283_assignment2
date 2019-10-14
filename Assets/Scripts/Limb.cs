8eaeb2b4aff34952f536d9151dc5df77887aac36
Andreas Garvik <andreas.garvik13@gmail.com>
2019-10-15 01:22:27 +1000

    :sparkles:

---
+++ Assets/Scripts/Limb.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+public class Limb : MonoBehaviour
+{
+	public Limb child;
+	public Vector3 jointLocation;
+	public Vector3 jointOffset;
+	public float angle;
+	public float lastAngle;
+	public Vector3[] limbVertexLocations;
+	public Material material;
+	public Mesh mesh;
+
+	void Start()
+	{
+		DrawLimb();
+		if (child != null)
+		{
+			child.GetComponent<Limb>().MoveByOffset(jointOffset);
+		}
+	}
+
+	private void DrawLimb()
+	{
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+
+		mesh = GetComponent<MeshFilter>().mesh;
+
+		GetComponent<MeshRenderer>().material = material;
+
+		mesh.Clear();
+
+		mesh.vertices = limbVertexLocations;
+
+		mesh.colors = new Color[]
+		{
+							new Color(1.0f, 1.0f, 1.0f, 1.0f),
+							new Color(1.0f, 1.0f, 1.0f, 1.0f),
+							new Color(1.0f, 1.0f, 1.0f, 1.0f),
+							new Color(1.0f, 1.0f, 1.0f, 1.0f)
+		};
+
+		mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+	}
+
+	public void MoveByOffset(Vector3 offset)
+	{
+		Matrix3x3 T = Matrix3x3.Translate(offset);
+		Vector3[] vertices = mesh.vertices;
+
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] = T.MultiplyPoint(vertices[i]);
+		}
+		mesh.vertices = vertices;
+
+		jointLocation = T.MultiplyPoint(jointLocation);
+
+		if (child != null)
+		{
+			child.GetComponent<Limb>().MoveByOffset(offset);
+		}
+	}
+
+	public void RotateAroundPoint(Vector3 point, float angle, float lastAngle)
+	{
+		Matrix3x3 T1 = Matrix3x3.Translate(-point);
+
+		Matrix3x3 R1 = Matrix3x3.Rotate(-lastAngle);
+
+		Matrix3x3 T2 = Matrix3x3.Translate(point);
+
+		Matrix3x3 R2 = Matrix3x3.Rotate(angle);
+
+		Matrix3x3 M = T2 * R2 * R1 * T1;
+
+		Vector3[] vertices = mesh.vertices;
+
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] = M.MultiplyPoint(vertices[i]);
+		}
+		mesh.vertices = vertices;
+
+		jointLocation = M.MultiplyPoint(jointLocation);
+
+		if (child != null)
+		{
+			child.GetComponent<Limb>().RotateAroundPoint(point, angle, lastAngle);
+		}
+
+	}
+}
---
+++ Assets/Scripts/QutJr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QutJr : MonoBehaviour
+{
+	public GameObject limb;
+	public int limbsCount = 4;
+	public Material material;
+	private Limb[] limbs;
+	void Start()
+	{
+		limbs = new Limb[4];
+		Limb l1 = Instantiate(limb).GetComponent<Limb>();
+		l1.jointLocation = new Vector3(0, 0, 1);
+		l1.jointOffset = new Vector3(0, 2, 1);
+		l1.limbVertexLocations = new Vector3[] {
+						new Vector3(-0.1f,0,1),
+						new Vector3(-0.1f,1,1),
+						new Vector3(0.1f,1,1),
+						new Vector3(0.1f,0,1)
+		};
+		l1.material = material;
+		limbs[0] = l1;
+		Limb l2 = Instantiate(limb).GetComponent<Limb>();
+		l2.jointLocation = new Vector3(0, 2, 1);
+		l2.jointOffset = new Vector3(0, 2, 1);
+		l2.limbVertexLocations = new Vector3[] {
+						new Vector3(-0.25f,0,1),
+						new Vector3(-0.25f,2,1),
+						new Vector3(0.25f,2,1),
+						new Vector3(0.25f,0,1)
+		};
+		l2.material = material;
+		l2.angle = 3;
+		l2.child = l1;
+		limbs[1] = l2;
+		Limb l3 = Instantiate(limb).GetComponent<Limb>();
+		l3.jointLocation = new Vector3(0, 0, 1);
+		l3.jointOffset = new Vector3(0, 2, 1);
+		l3.limbVertexLocations = new Vector3[] {
+						new Vector3(-0.25f,0,1),
+						new Vector3(-0.25f,2,1),
+						new Vector3(0.25f,2,1),
+						new Vector3(0.25f,0,1)
+		};
+		l3.material = material;
+		l3.child = l2;
+		limbs[2] = l3;
+		Limb l4 = Instantiate(limb).GetComponent<Limb>();
+		l4.jointLocation = new Vector3(0, 0, 1);
+		l4.jointOffset = new Vector3(0, 0, 0);
+		l4.limbVertexLocations = new Vector3[] {
+						new Vector3(-1,-1,1),
+						new Vector3(-1,0,1),
+						new Vector3(1,0,1),
+						new Vector3(1,-1,1)
+		};
+		l4.material = material;
+		l4.child = l3;
+		limbs[3] = l4;
+	}
+
+	void Update()
+	{
+
+	}
+}
